470cd32d34b39f4017e406bce5e4e888568ff651
Adriana Ayllon <ayllon814@gmail.com>
2026-04-01 20:20:27 -0400

    tarea 1 cambios

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ import requests
 from django.http import JsonResponse
 
 def obtener_posts(request):
-    url = "https://jsonplaceholder.typicode.com/posts"
+    url = "https://picsum.photos/v2/list"
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data, safe=False)
